a9f80778ee5f0148b7bd9bef7d71b2af6bd9594a
MahsaHashempoor <ha.mahsa73@gmail.com>
2024-02-06 15:25:15 +0000

    initial

---
+++ Chapter_2_Language_Basic/_01_ConvertFeetToInches_ValidInput_ReturnsCorrectResult.cs
@@ -0,0 +1,27 @@
+namespace Chapter_2_Language_Basic
+{
+    public class FeetConverterTests
+    {
+        [Theory]
+        [InlineData(0, 0)] // Test case for 0 feet
+        [InlineData(1, 12)] // Test case for 1 foot
+        [InlineData(5, 60)] // Test case for 5 feet
+        [InlineData(100, 1200)] // Test case for 10 feet
+        [InlineData(30, 360)] // Test case for 30 feet
+        public void ConvertFeetToInches_ValidInput_ReturnsCorrectResult(int feet, int expectedInches)
+        {
+            // Arrange: Create a method to convert feet to inches
+            int FeetToInches(int feet)
+            {
+                int inches = feet * 12;
+                return inches;
+            }
+            //Act 
+            int actualInches = FeetToInches(feet);
+
+            // Assert: Check if the result matches the expected value
+            Assert.Equal(expectedInches, actualInches);
+
+        }
+    }
+}
