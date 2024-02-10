ce03e5c724490c92012710cb2b2bf82e532c0602
MahsaHashempoor <ha.mahsa73@gmail.com>
2024-02-10 15:59:11 +0000

    init setter

---
+++ Chapter_3_Creating_Types/_01_Properties.cs
@@ -0,0 +1,26 @@
+namespace Chapter_3_Creating_Types
+{
+    public class Note
+    {
+        public int Pitch { get; init; }
+        public int Duration { get; init; }
+    }
+    public class PropertiesTestClass
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+            Note note = new Note { Pitch=50};
+
+            //Act
+
+            //note.Pitch = 60; // Error – init-only setter!
+
+            //Assert
+
+            Assert.Equal(50, note.Pitch);
+
+        }
+    }
+}
